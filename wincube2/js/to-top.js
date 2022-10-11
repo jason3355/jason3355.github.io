525308d7c7ea749c46878b19b8a8f19066ccda6f
jason3355 <71420235+jason3355@users.noreply.github.com>
2022-10-11 22:12:51 +0800

    Add files via upload

---
+++ wincube2/js/to-top.js
@@ -0,0 +1,28 @@
+$(function () {
+	// close效果
+	$(".jq-adClose").click(function (event) {
+		/* Act on the event */
+		event.preventDefault();
+		$(".adBox").fadeOut();
+	});
+
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 200) {
+			if ($(".goTop").hasClass("hide")) {
+				$(".goTop").toggleClass("hide");
+			}
+		} else {
+			$(".goTop").addClass("hide");
+		}
+	});
+
+	$(".jq-goTop").click(function (e) {
+		e.preventDefault();
+		$("html,body").animate(
+			{
+				scrollTop: 0
+			},
+			1000
+		);
+	});
+});
